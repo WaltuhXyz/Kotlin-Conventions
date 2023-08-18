af6c438ac0e0c59072246b9b483a13088d903fd0
Dmitry Lomov <drupal.doesnot.exists@mail.ru>
2023-08-18 08:47:48 +0300

    fix(Kotlin-Plugins): Apply the Dokka plugin only to the Kt-Lib plugin
    Move the Dokka plugin configuration from the base plugin to the lib plugin because javadoc is really only needed for the libraries and APIs.

--- src/main/kotlin/kt-lib.gradle.kts
+++ src/main/kotlin/kt-lib.gradle.kts
@@ -1,6 +1,18 @@
 import org.gradle.kotlin.dsl.`java-library`
+import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
     id("kt-base")
     `java-library`
+
+    id("org.jetbrains.dokka")
+}
+
+val dokkaTask = tasks.getByName<DokkaTask>("dokkaJavadoc")
+tasks {
+    create<Jar>("javadocJar") {
+        dependsOn(dokkaTask)
+        from(dokkaTask)
+        archiveClassifier.set("javadoc")
+    }
 }
