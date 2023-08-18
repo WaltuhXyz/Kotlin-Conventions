8b73267db4639c1ded8a9be85ea436997316d725
Dmitry Lomov <drupal.doesnot.exists@mail.ru>
2023-08-18 09:49:51 +0300

    fix(gradle): Reconfigure gradle plugin publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,32 +14,6 @@ repositories {
 gradlePlugin {
     website.set("https://github.com/WaltuhXyz/Kotlin-Conventions")
     vcsUrl.set("https://github.com/WaltuhXyz/Kotlin-Conventions")
-    plugins {
-        create("kt-base") {
-            id = "xyz.waltuh.gradle.kt-base"
-            displayName = "Kotlin Base"
-            description = "Common conventions for all the Kotlin JVM projects"
-            tags.set(setOf("kotlin", "jvm", "convention"))
-        }
-        create("kt-lib") {
-            id = "xyz.waltuh.gradle.kt-lib"
-            displayName = "Kotlin Library"
-            description = "Common conventions for all the Kotlin JVM libraries"
-            tags.set(setOf("kotlin", "jvm", "convention", "library"))
-        }
-        create("kt-app") {
-            id = "xyz.waltuh.gradle.kt-app"
-            displayName = "Kotlin Application"
-            description = "Common conventions for all the Kotlin JVM applications"
-            tags.set(setOf("kotlin", "jvm", "convention", "application"))
-        }
-        create("kt-app-shaded") {
-            id = "xyz.waltuh.gradle.kt-app"
-            displayName = "Kotlin Fat-Jar Application"
-            description = "Common conventions for all the Kotlin JVM applications that shade their dependencies"
-            tags.set(setOf("kotlin", "jvm", "convention", "application", "shadow"))
-        }
-    }
 }
 
 dependencies {
